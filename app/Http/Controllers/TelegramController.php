83ab06f1adb42d31b62bd7b104d86fa34b458ba6
mmskazak <mmskazak@gmail.com>
2023-10-21 22:56:51 +0300

    buttons is work

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Telegram\Bot\Keyboard\Keyboard;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
+
 class TelegramController extends Controller
 {
     public function webhook(Request $request)
@@ -13,13 +14,11 @@ class TelegramController extends Controller
         $update = Telegram::commandsHandler(true);
 
         $keyboard = [
-            ['7', '8', '9'],
-            ['4', '5', '6'],
-            ['1', '2', '3'],
-            ['0']
+            ['Профиль', 'Купить VPN'],
+            ['Информация']
         ];
 
-        $reply_markup = Telegram::replyKeyboardMarkup([
+        $reply_markup = Keyboard::make([
             'keyboard' => $keyboard,
             'resize_keyboard' => true,
             'one_time_keyboard' => true
